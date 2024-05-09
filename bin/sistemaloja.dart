77ef491bcebabb0ee2c3bcb70c7f3f3e53723c78
Letícia Borges <leticinhabb4@gmail.com>
2024-05-09 00:22:39 -0300

    Ajuste class CarrinhoDeCompras

--- bin/sistemaloja.dart
+++ bin/sistemaloja.dart
@@ -1,59 +1,92 @@
 import 'dart:io';
-void main(){
-Produto produto = Produto(0012, 'cor escura' , 2.90);
-produto.printProduto();
+
+void main() {
+  Produto produto = Produto(0012, 'cor escura', 2.90);
+  produto.printProduto();
+  ProdutoFisico produtoFisico = ProdutoFisico(2.0, 0013, 'cor clara', 3.50);
+  produtoFisico.CalcularFrete();
+
+  ProdutoVirtual produtoVirtual = ProdutoVirtual(5, false, 009, 'cor neutra', 4.20);
+  produtoVirtual.VerificarEnvioGratis();
+
+  CarrinhoDeCompras carrinho = CarrinhoDeCompras();
+  carrinho.addProdutos(produto);
+  carrinho.addProdutos(produtoFisico);
+  carrinho.addProdutos(produtoVirtual);
+  carrinho.ValorTotal();
 }
-class Produto implements CarrinhoDeCompras {
+// class Pai produto
+class Produto {
   int codigo;
   String descricao;
   double preco;
+
   Produto(this.codigo, this.descricao, this.preco);
-  void printProduto(){
-    print('o produto tem o $codigo com a descricao $descricao no valor de $preco em reais');
-  }
-  @override
-  void addProdutos(){
-  }
-  @override
-  void ValorTotal() {
 
+  void printProduto() {
+    print(
+        'o produto tem o $codigo com a descricao $descricao no valor de $preco em reais');
   }
-  @override
-  void removeProdutos() {
+  void add(Produto produto) {}
 
-  }
+  void remove(Produto produto) {}
 }
-class ProdutoFisico extends Produto{
+
+class ProdutoFisico extends Produto {
   double peso;
-  ProdutoFisico(this.peso, int codigo, String descricao, double preco): super (codigo, descricao, preco);
-  void CalcularFrete(){
+
+  ProdutoFisico(this.peso, int codigo, String descricao, double preco)
+      : super(codigo, descricao, preco);
+// método para calcular o Frete - recebe dados do usuario
+  void CalcularFrete() {
     print('Qual é a distancia em KM?');
     String? inputDis = stdin.readLineSync();
-    if(inputDis != null){
+    if (inputDis != null) {
       int Distancia = int.parse(inputDis);
       double? valorFrete = funcFrete(Distancia);
       print('o valor do frete do produto é $valorFrete');
     }
   }
-  double? funcFrete(int Distancia){
-    return
-    Distancia * 0.01;
+// função para calcular o frete
+  double? funcFrete(int Distancia) {
+    return Distancia * 0.01;
   }
 }
-class ProdutoVirtual extends Produto{
+
+class ProdutoVirtual extends Produto {
   int tamanho;
   bool isEnvioGratis;
-  ProdutoVirtual(this.tamanho, this.isEnvioGratis, int codigo, String descricao, double preco): super (codigo, descricao, preco);
-  void VerificarEnvioGratis(){
-    if(tamanho <= 10){
+
+  ProdutoVirtual(this.tamanho, this.isEnvioGratis, int codigo, String descricao,
+      double preco)
+      : super(codigo, descricao, preco);
+
+  void VerificarEnvioGratis() {
+    if (tamanho <= 10) {
       isEnvioGratis = true;
-    }else{
+      print('O envio é grátis.');
+    } else {
       isEnvioGratis = false;
+      print('O envio não é grátis você irá pagar.');
     }
   }
 }
-abstract class CarrinhoDeCompras{
-  void addProdutos();
-  void removeProdutos();
-  void ValorTotal();
+
+class CarrinhoDeCompras {
+  List<dynamic> produtos = [];
+
+  void addProdutos(Produto produto) {
+    produtos.add(produto);
+  }
+
+  void ValorTotal() {
+  double total = 0;
+  for (var p in produtos) {
+  total += p.preco;
+  }
+  print('O valor total dos produtos é: $total');
+}
+  void removeProdutos(Produto produto) {
+    produto.remove(produto);
+  }
 }
